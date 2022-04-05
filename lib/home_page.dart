17eec66ed72d6c60f0ac58965514c2e42986131a
Haffsriyandra Yusuf <haffsriyandrayusuf@protonmail.com>
2022-04-05 13:50:45 +0700

    menambahkan navigation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -64,6 +64,76 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 30,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: const [
+                      Icon(
+                        Icons.call,
+                        color: Color(0xff00A3FF),
+                        size: 18,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'CALL',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Color(0xff00A3FF),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      Icon(
+                        Icons.near_me,
+                        color: Color(0xff00A3FF),
+                        size: 18,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'ROUTE',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Color(0xff00A3FF),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      Icon(
+                        Icons.share,
+                        color: Color(0xff00A3FF),
+                        size: 18,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'SHARE',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Color(0xff00A3FF),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
